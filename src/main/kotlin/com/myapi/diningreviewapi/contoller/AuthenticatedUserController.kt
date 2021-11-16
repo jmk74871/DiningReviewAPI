74d2b9005e4afef30f215071afa7612098125693
jmk74871 <71671622+jmk74871@users.noreply.github.com>
2021-11-16 21:56:48 +0100

    Added Endpoint to delete reviews.

--- src/main/kotlin/com/myapi/diningreviewapi/contoller/AuthenticatedUserController.kt
+++ src/main/kotlin/com/myapi/diningreviewapi/contoller/AuthenticatedUserController.kt
@@ -64,18 +64,48 @@ class AuthenticatedUserController(
         return this.diningReviewRepository.save(reviewOriginal)
     }
 
+    @DeleteMapping("/reviews/{reviewID}")
+    fun deleteReview(@PathVariable reviewID: Long,
+                     @CookieValue("token", defaultValue="") tokenUuidString: String) {
+        // validate token
+        val user = validateToken(tokenUuidString)
+
+        // get review by id
+        val reviewOptional = this.diningReviewRepository.findById(reviewID)
+        if(reviewOptional.isEmpty) throw ResponseStatusException(HttpStatus.BAD_REQUEST, " Could not find a review corresponding to the ID provided.")
+        val reviewOriginal = reviewOptional.get()
+
+        // check if updating user is same as submitting user
+        val tokenOptional = this.tokenRepository.findByUuidString(tokenUuidString)
+        if(tokenOptional.isEmpty) throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        val token = tokenOptional.get()
+        if (token.userID != user.id) throw ResponseStatusException(HttpStatus.FORBIDDEN)
+
+        // delete review
+        return this.diningReviewRepository.delete(reviewOriginal)
+    }
+
     // Endpoints regarding restaurants
     @PostMapping("/")
     fun addRestaurants(@RequestBody restaurant: Restaurant,
                        @CookieValue("token", defaultValue="") tokenUuidString: String): Restaurant {
         // validate token
-        val user = validateToken(tokenUuidString)
+        validateToken(tokenUuidString)
+        /*
+        ToDO: check for duplicate submissions?
+         */
 
         return restaurantRepository.save(restaurant)
     }
 
+    /*
+    ToDo: Who may update restaurant entries or delete them?
+     */
+
 
     // Helper methods
+    // fun validateUserIsOriginalCreator
+
     fun validateToken(tokenUuidString: String): User {
         // check for empty inputString
         if(tokenUuidString == "") throw ResponseStatusException(HttpStatus.FORBIDDEN)
@@ -85,18 +115,17 @@ class AuthenticatedUserController(
 
         // check if the submitted tokenString belongs to a valid token
         val tokenOptional = this.tokenRepository.findByUuidString(tokenUuidString)
-        if(tokenOptional.isPresent){
-            val token = tokenOptional.get()
+        if(tokenOptional.isEmpty) throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        val token = tokenOptional.get()
 
-            // update timestamp
-            token.timeStamp = Instant.now().epochSecond
-            this.tokenRepository.save(token)
+        // update timestamp
+        token.timeStamp = Instant.now().epochSecond
+        this.tokenRepository.save(token)
 
-            val userOptional = token.userID?.let { this.userRepository.findById(it) } ?: throw ResponseStatusException(HttpStatus.FORBIDDEN)
-            if (userOptional.isEmpty) throw ResponseStatusException(HttpStatus.FORBIDDEN)
-            else return userOptional.get()
-
-        }else throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        // get user object and return it
+        val userOptional = token.userID?.let { this.userRepository.findById(it) } ?: throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        if (userOptional.isEmpty) throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        else return userOptional.get()
     }
 
     fun removeTimedOutTokens(){
